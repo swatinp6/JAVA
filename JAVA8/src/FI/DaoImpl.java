dda456a9ff5372e6da53e63ee680e6b9a369a88d
swatinp6 <swatinpatil6@gmail.com>
2024-07-05 12:41:41 +0530

    java

---
+++ JAVA8/src/FI/DaoImpl.java
@@ -0,0 +1,15 @@
+@FunctionalInterface
+interface DaoI{
+    public abstract void login();
+    void logout();
+}
+class DaoImpl implements DaoI{
+    public void login(){
+        System.out.println("login success");
+    }
+
+    public static void main(String[] args) {
+        DaoI dao=new DaoImpl();
+        dao.login();
+    }
+}
